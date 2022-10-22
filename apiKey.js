fb55df16f4ec1f6a4ea2d07f96fb1b0f6decd17e
Ryan-Then <48986079+Ryan-Then@users.noreply.github.com>
2022-10-23 01:33:55 +0800

    Update apiKey.js

--- apiKey.js
+++ apiKey.js
@@ -1,4 +1,4 @@
 /* Copy this into a file named apiKey.js */
-const apiKey = '1Gxba9bMHt24g3KfFhMzkC042uymjmKlvqapcDCf9';
+const apiKey = '12rznt0txuxhcq9ECoBmdHGjv536bE2LluP4xfEGd';
 const appId = 'my.edu.mmu.student.1191302688.microverse';
 export default {apiKey, appId};
