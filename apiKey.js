534f450628bb244b256f7bdd3192c5d1fad8e38f
Ryan-Then <48986079+Ryan-Then@users.noreply.github.com>
2022-10-23 01:31:42 +0800

    Add files via upload

---
+++ apiKey.js
@@ -0,0 +1,4 @@
+/* Copy this into a file named apiKey.js */
+const apiKey = '1Gxba9bMHt24g3KfFhMzkC042uymjmKlvqapcDCf9';
+const appId = 'my.edu.mmu.student.1191302688.microverse';
+export default {apiKey, appId};
