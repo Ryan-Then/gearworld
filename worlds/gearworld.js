89b795eedf4bf56af2814d6e8d33d6775902917e
Ryan-Then <48986079+Ryan-Then@users.noreply.github.com>
2023-05-17 00:19:47 +0800

    Add files via upload
    latest gearworld file added

--- worlds/gearworld.js
+++ worlds/gearworld.js
@@ -6,7 +6,7 @@ export function init(Constants) {
 	
     Constants.UserBehaviorDirectory = "behaviors/gearworld";
     Constants.UserBehaviorModules = [
-        "lights.js", "drawing.js", "gridFloor.js", "earth.js", "createCuboid.js", "createSphere.js", "createCylinder.js", "cascade.js", "rapier.js", "gearSpin.js", "gearCounterSpin.js"
+        "lights.js", "drawing.js", "gridFloor.js", "earth.js", "createCuboid.js", "createSphere.js", "createCylinder.js", "cascade.js", "rapier.js", "gearSpin.js", "gearCounterSpin.js", "sticky.js"
     ];
 	
     Constants.UseRapier = true;
@@ -30,31 +30,35 @@ export function init(Constants) {
                 shadow: true,
             }
         },
-        {
-            card: {
-				type: "3d",
-				modelType: "glb",
-				name: "biggear",
-                dataLocation: "./assets/3D/biggear.glb",
-				rotation: [0, 0, 0],
-				translation: [0, 2, 0],
-				behaviorModules: ["gearCounterSpin"],
-				scale: [0.5, 0.5, 0.5],
-            }
-        },
-        {
-            card: {
-				type: "3d",
-				modelType: "glb",
-				name: "smallgear",
-                dataLocation: "./assets/3D/smallgear.glb",
-				rotation: [0, 0, 0],
-				color: 0xaa6666,
-				translation: [0, 2, -1.17],
-				behaviorModules: ["gearSpin"],
-				scale: [0.45, 0.45, 0.45],
-            }
-        },
+		
+		
+		
+		/* {		
+		card: {
+            name: 'menu label',
+			
+			translation: [3, 0, 1.5],
+			rotation: [0, -Math.PI / 2, 0],
+            depth: 0.05,
+			type: "text",
+			runs: [{text: "Thickness\n\nEnable Bevel\n\nBevel Thickness\n\nBevel Size\n"}],
+			margins: {left: 20, top: 20, right: 20, bottom: 20},
+			backgroundColor: 0x888888,
+			color: 0x000000,
+			//color: 0xf4e056,
+			frameColor: frameColor,
+			width: 0.8,
+			height: 0.7,
+			textScale: 0.002,
+			shadow: true,
+			}
+		}, */
+
+        
+
+			
+	
+        
 		{
             card: {
                 name: "drawing",
@@ -81,71 +85,7 @@ export function init(Constants) {
             }
         },
 
-        {
-            card: {
-                name: "portal button",
-                translation: [0.5, 0.366177949493676, -2.566177949493676],
-                behaviorModules: ["OpenRefineryPortalButton"],
-				type: "object",
-            }
-        },
-        {
-            card: {
-		        name: "cuboid label",
-				translation: [0.5, 0, -2.566177949493676],
-				type: "2d",
-                textureType: "image",
-                textureLocation: "./assets/images/cuboid.png",
-                cardHilite: 0xffffaa,
-                fullBright: true,
-                cornerRadius: 0.05,
-                depth: 0.05,
-                shadow: true,
-            }
-        },		
-        {
-            card: {
-                name: "portal button sphere",
-                translation: [2.5, 0.366177949493676, -2.566177949493676],
-                behaviorModules: ["OpenRefineryPortalButtonSphere"],
-                type: "object",
-            }
-        },
-        {
-            card: {
-		        name: "sphere label",
-				translation: [2.5, 0, -2.566177949493676],
-				type: "2d",
-                textureType: "image",
-                textureLocation: "./assets/images/sphere.png",
-                cardHilite: 0xffffaa,
-                fullBright: true,
-                cornerRadius: 0.05,
-                depth: 0.05,
-                shadow: true,
-            }
-        },		
-        {
-            card: {
-                name: "portal button cylinder",
-                translation: [-1.5, 0.366177949493676, -2.566177949493676],
-                behaviorModules: ["OpenRefineryPortalButtonCylinder"],
-                type: "object",
-            }
-        },
-        {
-            card: {
-		        name: "cylinder label",
-				translation: [-1.5, 0, -2.566177949493676],
-				type: "2d",
-                textureType: "image",
-                textureLocation: "./assets/images/cylinder.png",
-                cardHilite: 0xffffaa,
-                fullBright: true,
-                cornerRadius: 0.05,
-                depth: 0.05,
-                shadow: true,
-            }
-        }		
+        	
+        	
     ];
 }
